5518d8f9750c88b9e7e3148572b4cd028e51646f
Kanybek Tolegen <kanybek.tolegen@berekebank.kz>
2025-06-17 12:34:28 +0500

    fix: small fix

--- src/components/icon-input/index.tsx
+++ src/components/icon-input/index.tsx
@@ -1,8 +1,10 @@
 import { useState, type ChangeEvent } from "react"
 import { SearchIcon } from "../../assets/icons"
 import styles from './style.module.scss'
+import { useIsMobile } from "../../hooks"
 
 export const IconInput = ({ onChange }: { onChange: (value: string) => void }) => {
+    const isMobile = useIsMobile();
     const [value, setValue] = useState("")
 
     const handleInputChange = (e: ChangeEvent<HTMLInputElement>) => {
@@ -13,7 +15,7 @@ export const IconInput = ({ onChange }: { onChange: (value: string) => void }) =
 
     return <div className={styles["icon-input"]}>
         <div className={styles["input-wrapper"]}>
-            <input value={value} className={styles.input} placeholder="Поиск" onChange={handleInputChange} />
+            <input value={value} className={`${styles.input} ${isMobile ? styles["input__mobile"] : ""}`} placeholder="Поиск" onChange={handleInputChange} />
             <SearchIcon />
         </div>
     </div>
